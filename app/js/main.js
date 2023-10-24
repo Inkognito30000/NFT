c115d319db339e682404bcda16ef01e145091282
Inkognito30000 <andreymarkov435@yandex.ru>
2023-10-24 15:42:35 +0300

    доделал планшетную версию адаптива

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,9 @@
 "use strict";
 
+const burger = document.querySelector('.header-burger');
+const nav = document.querySelector('.nav__items');
+
+burger.addEventListener('click', function() {
+    this.classList.toggle('active');
+    nav.classList.toggle('open');
+})
